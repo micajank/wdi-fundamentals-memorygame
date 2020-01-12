24c10f7c4c275b5b09dc3deaf0bd570d2bab5d06
micajank <micajank@umich.edu>
2020-01-12 13:32:09 -0500

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "king";
+
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
