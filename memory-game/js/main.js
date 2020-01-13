c5f8b44702701eacb7a1f134e503c502904d27de
micajank <micajank@umich.edu>
2020-01-13 14:09:07 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -3,24 +3,33 @@
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-let cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+			alert("You found a match!");
 	}
 	else {
 		alert("Sorry, try again.")
 	}
 }
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+}
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+
+
+
+
+
+
